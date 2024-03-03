ae6f790d481e33310f1573e1b77caa010f8b9175
Adzzio <105452171+Adzzio@users.noreply.github.com>
2024-03-03 12:53:11 +0100

    OK

--- Testy.py
+++ Testy.py
@@ -1,5 +1,6 @@
 import unittest
-
+# Importuj funkcję, którą chcesz przetestować
+from Funkcje_Baza import zamien_przecinek_na_kropke
 # Importuj funkcję, którą chcesz przetestować
 from Funkcje_Baza import convert_to_dict
 
@@ -34,6 +35,41 @@ class TestConvertToDictFunction2(unittest.TestCase):
             {"tow_kod": 2, "tow_name": 'Product2', "ilo_is": 20, "ce": 30.0},
         ]
         self.assertEqual(result, expected_result)
+
+    import unittest
+
+
+
+class TestZamienPrzecinekNaKropkeFunction(unittest.TestCase):
+
+    def test_zamien_przecinek_na_kropke(self):
+        # Przygotuj dane wejściowe
+        input_text_with_comma = "10,5"
+        input_text_without_comma = "20.3"
+
+        # Wywołaj funkcję, którą chcesz przetestować
+        result_with_comma = zamien_przecinek_na_kropke(input_text_with_comma)
+        result_without_comma = zamien_przecinek_na_kropke(input_text_without_comma)
+
+        # Sprawdź, czy funkcja zwraca oczekiwane wyniki
+        expected_result_with_comma = "10.5"
+        expected_result_without_comma = "20.3"
+
+        self.assertEqual(result_with_comma, expected_result_with_comma)
+        self.assertEqual(result_without_comma, expected_result_without_comma)
+
+    def test_zamien_przecinek_na_kropke_no_comma(self):
+        # Przygotuj dane wejściowe bez przecinka
+        input_text = "15.7"
+
+        # Wywołaj funkcję, którą chcesz przetestować
+        result = zamien_przecinek_na_kropke(input_text)
+
+        # Sprawdź, czy funkcja zwraca dokładnie to samo, gdy nie ma przecinka
+        self.assertEqual(result, input_text)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
